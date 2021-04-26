b07426eb14128f0c0f5a47fd55559c74290e7b24
Kyoya Yamaguchi <kyoryu32@gmail.com>
2021-04-26 14:14:12 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Yamaguchi Kyoya on 2021/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

e78e74eed8c8ec1b77b8929149e01a0b1c469f64
Kyoya Yamaguchi <kyoryu32@gmail.com>
2021-05-12 20:39:53 +0900

    challenge task

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -10,17 +10,35 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var profileLabel: UILabel!
+    @IBOutlet var profileCommentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.isHidden = true
-        nameLabel.isHidden = true
     }
 
-    @IBAction func didTapButton() {
-        imageView.isHidden = false
-        nameLabel.isHidden = false
+    @IBAction func didTapButton1() {
+        imageView.image = UIImage(named: "philImage")
+        profileLabel.text = "名前"
+        profileCommentLabel.text = "メンターのphill"
+    }
+    
+    @IBAction func didTapButton2() {
+        imageView.image = UIImage(named: "trackImage")
+        profileLabel.text = "スポーツ"
+        profileCommentLabel.text = "陸上が好き"
+    }
+    
+    @IBAction func didTapButton3() {
+        imageView.image = UIImage(named: "appleImage")
+        profileLabel.text = "好きな食べ物"
+        profileCommentLabel.text = "りんごが好き"
+    }
+    
+    @IBAction func didTapButton4() {
+        imageView.image = UIImage(named: "flightImage")
+        profileLabel.text = "趣味"
+        profileCommentLabel.text = "飛行機に乗ること"
     }
 
 }
